7ad70e6582101b1b0f50c6d6f940a9fdf16b9b83
Martin Graversen <martin@graversen.io>
2018-06-16 18:50:27 +0200

    Player chat console message regex stuff

--- src/main/java/io/graversen/rust/rcon/ConsoleToEventDigester.java
+++ src/main/java/io/graversen/rust/rcon/ConsoleToEventDigester.java
@@ -1,38 +1,66 @@
 package io.graversen.rust.rcon;
 
-import io.graversen.rust.rcon.events.ChatMessageEvent;
-import io.graversen.rust.rcon.events.PlayerConnectedEvent;
-import io.graversen.rust.rcon.events.PlayerDeathEvent;
-import io.graversen.rust.rcon.events.PlayerDisconnectedEvent;
+import io.graversen.rust.rcon.events.*;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ConsoleToEventDigester
 {
+    private final Pattern squareBracketMatcher = Pattern.compile("\\[(.*?)]");
+
     public Optional<ConsoleDigests> digest(String consoleInput)
     {
         return Arrays.stream(ConsoleDigests.values()).filter(x -> x.matches(consoleInput)).findFirst();
     }
 
-    public ChatMessageEvent digestChatMessageEvent()
+    public ChatMessageEvent digestChatMessageEvent(String consoleInput)
     {
+        // [CHAT] Pope of the Nope[468295/76561197979952036] : ssss
+        validateEvent(consoleInput, ConsoleDigests.CHAT);
+
+        final String leftHandString = consoleInput.split(":")[0];
+        final String chatMessage = consoleInput.split(":")[1].trim();
+
+        final Matcher matcher = squareBracketMatcher.matcher(leftHandString);
+        final String steamId64 = matcher.group(2).split("/")[1];
+
+        final var chatMessageEvent = new ChatMessageEvent(, , chatMessage);
+
         return null;
     }
 
-    public PlayerConnectedEvent digestPlayerConnectedEvent()
+    public PlayerConnectedEvent digestPlayerConnectedEvent(String consoleInput)
     {
+        // 82.102.20.179:52298/76561197979952036/Pope of the Nope joined [windows/76561197979952036]
         return null;
     }
 
-    public PlayerDeathEvent digestPlayerDeathEvent()
+    public PlayerDeathEvent digestPlayerDeathEvent(String consoleInput)
     {
+        // Jiji[3831334/76561198189196679] was killed by [4267870/8141844]
         return null;
     }
 
-    public PlayerDisconnectedEvent digestPlayerDisconnectedEvent()
+    public PlayerKillEvent digestPlayerKillEvent(String consoleInput)
     {
+        // [4267870/8141844] was killed by Jiji[3831334/76561198189196679]
         return null;
+    }
+
+    public PlayerDisconnectedEvent digestPlayerDisconnectedEvent(String consoleInput)
+    {
+        // 82.102.20.179:61714/76561197979952036/Pope of the Nope disconnecting: closing
+        return null;
+    }
+
+    private void validateEvent(String consoleInput, ConsoleDigests consoleDigest)
+    {
+        this.digest(consoleInput)
+                .map(c -> c.equals(consoleDigest))
+                .orElseThrow(() -> new IllegalArgumentException("Invalid event type for console input"));
     }
 
     private enum ConsoleDigests implements IDigest
