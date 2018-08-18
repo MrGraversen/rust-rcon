a590e6da6b8802bd95923374fd7314d46212b5f4
Martin Graversen <martin@graversen.io>
2018-08-18 14:46:00 +0200

    AI rcon implementation

--- src/main/java/io/graversen/rust/rcon/Rcon.java
+++ src/main/java/io/graversen/rust/rcon/Rcon.java
@@ -3,12 +3,15 @@ package io.graversen.rust.rcon;
 public class Rcon
 {
     private final IRconClient rconClient;
+
     private final InventoryRcon inventoryRcon;
+    private final AiRcon aiRcon;
 
     public Rcon(IRconClient rconClient)
     {
         this.rconClient = rconClient;
         this.inventoryRcon = new InventoryRcon();
+        this.aiRcon = new AiRcon();
     }
 
     public void kick(String player, String reason)
@@ -34,6 +37,11 @@ public class Rcon
         return inventoryRcon;
     }
 
+    public AiRcon ai()
+    {
+        return aiRcon;
+    }
+
     private class InventoryRcon
     {
         public void giveTo(String player, String itemShortName, int amount)
@@ -51,6 +59,21 @@ public class Rcon
         public void giveAll(String itemShortName, int amount)
         {
             final String command = String.format("inventory.giveall \"%s\" \"%d\"", itemShortName, amount);
+            rconClient.sendRaw(command);
+        }
+    }
+
+    private class AiRcon
+    {
+        public void think(boolean think)
+        {
+            final String command = String.format("ai.think %s", String.valueOf(think));
+            rconClient.sendRaw(command);
+        }
+
+        public void move(boolean move)
+        {
+            final String command = String.format("ai.move %s", String.valueOf(move));
             rconClient.sendRaw(command);
         }
     }
