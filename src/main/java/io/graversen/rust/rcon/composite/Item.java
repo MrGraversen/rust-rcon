e376f20a5c35e9189c68987aeb0955dcc4c97171
Martin Graversen <martin@graversen.io>
2018-09-13 20:25:00 +0200

    Loadout + item

---
+++ src/main/java/io/graversen/rust/rcon/composite/Item.java
@@ -0,0 +1,13 @@
+package io.graversen.rust.rcon.composite;
+
+public class Item
+{
+    private final String shortName;
+    private final int amount;
+
+    public Item(String shortName, int amount)
+    {
+        this.shortName = shortName;
+        this.amount = amount;
+    }
+}
--- src/main/java/io/graversen/rust/rcon/composite/Loadout.java
+++ src/main/java/io/graversen/rust/rcon/composite/Loadout.java
@@ -1,6 +1,32 @@
 package io.graversen.rust.rcon.composite;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Loadout
 {
+    private final String playerName;
+    private final List<Item> items;
 
+    public Loadout(String playerName)
+    {
+        this(playerName, new ArrayList<>());
+    }
+
+    public Loadout(String playerName, List<Item> items)
+    {
+        this.playerName = playerName;
+        this.items = items;
+    }
+
+    public String getPlayerName()
+    {
+        return playerName;
+    }
+
+    public Stream<Item> getItems()
+    {
+        return items.stream();
+    }
 }
