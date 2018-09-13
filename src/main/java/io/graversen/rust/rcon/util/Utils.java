7fcd8ce1f28e5361a0e88b9305fafc8457a0f288
Martin Graversen <martin@graversen.io>
2018-09-13 21:09:23 +0200

    Ordinal "index of" implementations

---
+++ src/main/java/io/graversen/rust/rcon/util/Utils.java
@@ -0,0 +1,20 @@
+package io.graversen.rust.rcon.util;
+
+public abstract class Utils
+{
+    public static int nthIndexOf(String text, char needle, int n)
+    {
+        for (int i = 0; i < text.length(); i++)
+        {
+            if (text.charAt(i) == needle)
+            {
+                n--;
+                if (n == 0)
+                {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
