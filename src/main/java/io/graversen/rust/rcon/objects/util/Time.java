2574a19c901742df87004090916b9703405f1036
Martin Graversen <martin@graversen.io>
2018-12-08 22:23:11 +0100

    Time#MORNING (When sun has come up fully)

--- src/main/java/io/graversen/rust/rcon/objects/util/Time.java
+++ src/main/java/io/graversen/rust/rcon/objects/util/Time.java
@@ -3,5 +3,6 @@ package io.graversen.rust.rcon.objects.util;
 public abstract class Time
 {
     public static final int MIDNIGHT = 0;
+    public static final int MORNING = 9;
     public static final int NOON = 12;
 }
