3d6f2761d4db1696c12fac17d4cb95bba0353daf
Martin Graversen <martin@graversen.io>
2019-01-20 13:28:49 +0100

    BaseWebSocketClient abstraction

--- src/main/java/io/graversen/rust/rcon/websocket/DefaultWebSocketClient.java
+++ src/main/java/io/graversen/rust/rcon/websocket/DefaultWebSocketClient.java
@@ -1,19 +1,36 @@
 package io.graversen.rust.rcon.websocket;
 
 import io.graversen.rust.rcon.RconException;
-import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
-public class DefaultWebSocketClient extends WebSocketClient implements IWebSocketClient
+public class DefaultWebSocketClient extends BaseWebSocketClient implements IWebSocketClient
 {
     private final IWebSocketListener webSocketListener;
+    private final String connectionUri;
+    private final String connectionUriMasked;
 
     public DefaultWebSocketClient(URI serverUri, IWebSocketListener webSocketListener)
     {
         super(serverUri);
         this.webSocketListener = webSocketListener;
+        this.connectionUri = String.format(WS_URI_TEMPLATE, serverUri.getHost(), serverUri.getPort(), serverUri.getPath());
+        this.connectionUriMasked = String.format(WS_URI_TEMPLATE_MASKED, serverUri.getHost(), serverUri.getPort());
+    }
+
+    public static DefaultWebSocketClient usingCredentialsAndListener(String hostname, String password, int port, IWebSocketListener webSocketListener)
+    {
+        try
+        {
+            final String uriCompiled = String.format(WS_URI_TEMPLATE, hostname, port, password);
+            return new DefaultWebSocketClient(new URI(uriCompiled), webSocketListener);
+        }
+        catch (URISyntaxException e)
+        {
+            throw new RconException("Could not construct server URI");
+        }
     }
 
     @Override
@@ -23,7 +40,7 @@ public class DefaultWebSocketClient extends WebSocketClient implements IWebSocke
     }
 
     @Override
-    public void onOpen(ServerHandshake handshakedata)
+    public void onOpen(ServerHandshake serverHandshake)
     {
         webSocketListener.onOpen();
     }
