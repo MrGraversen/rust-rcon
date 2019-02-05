899a71178a051c4c8cae8d90717318fe5634ab9d
Martin Graversen <martin@graversen.io>
2019-02-05 20:40:19 +0100

    Player joined osx test

--- src/test/java/io/graversen/rust/rcon/test/TestParsePlayerConnectedEvent.java
+++ src/test/java/io/graversen/rust/rcon/test/TestParsePlayerConnectedEvent.java
@@ -16,7 +16,8 @@ class TestParsePlayerConnectedEvent extends BaseDefaultParserTest
     private static final String[] messages = {
             "82.102.20.179:52298/76561197979952036/Pope of the Nope joined [windows/76561197979952036]",
             "79.193.40.58:55162/76561198845816557/m_7o7 joined [linux/76561198845816557]",
-            "92.171.193.37:52278/76561198063601715/Orion (EN/FR/ES) joined [windows/76561198063601715]"
+            "92.171.193.37:52278/76561198063601715/Orion (EN/FR/ES) joined [windows/76561198063601715]",
+            "62.203.160.70:61437/76561198896647223/dkbarragues22 joined [osx/76561198896647223]"
     };
 
     private final IEventParser<PlayerConnectedEvent> eventParser = new PlayerConnectedEventParser();
@@ -49,5 +50,17 @@ class TestParsePlayerConnectedEvent extends BaseDefaultParserTest
         assertEquals("windows", event.get().getOsDescriptor());
         assertEquals("76561198063601715", event.get().getSteamId64());
         assertEquals("92.171.193.37:52278", event.get().getConnectionTuple());
+    }
+
+    @Test
+    void test_findCorrectInfo_3()
+    {
+        final Optional<PlayerConnectedEvent> event = eventParser.parseEvent().apply(messages[3]);
+
+        assertTrue(event.isPresent());
+        assertEquals("dkbarragues22", event.get().getPlayerName());
+        assertEquals("osx", event.get().getOsDescriptor());
+        assertEquals("76561198896647223", event.get().getSteamId64());
+        assertEquals("62.203.160.70:61437", event.get().getConnectionTuple());
     }
 }
