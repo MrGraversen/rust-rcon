bfe4079d65564943e40456ab2754cdd64de76a61
Martin Graversen <martin@graversen.io>
2019-02-05 21:29:53 +0100

    Broadcast plugin, send to all, send to steam id64

--- src/main/umod/Broadcast.cs
+++ src/main/umod/Broadcast.cs
@@ -6,13 +6,23 @@ namespace Oxide.Plugins
 {
     [Info("Broadcast", "Martin Graversen", "0.0.1", ResourceId = 1)]
     [Description("Broadcast a message to the server")]
-    class EasyBroadcast : RustPlugin
+    class Broadcast : RustPlugin
     {
         [ConsoleCommand("broadcast")]
-        void broadcast(ConsoleSystem.Arg arg) {
+        private void cmdConsoleBroadcast(ConsoleSystem.Arg arg) {
+            if (arg.Connection != null || (arg.Args == null || arg.Args.Length == 0)) return;
+            PrintToChat(arg.Args[0]);
+        }
+
+        [ConsoleCommand("broadcastto")]
+        private void cmdConsoleBroadcastTo(ConsoleSystem.Arg arg) {
             if (arg.Connection != null || (arg.Args == null || arg.Args.Length == 0)) return;
 
-            // TODO!
+            var player = BasePlayer.FindByID(Convert.ToUInt64(arg.Args[0]));
+
+            if (player != null) {
+                PrintToChat(player, arg.Args[1]);
+            }
         }
     }
 }
