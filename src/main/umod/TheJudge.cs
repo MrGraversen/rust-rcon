25e59982e2cfa1dfe402d05ba40b9a25edb0483a
Martin Graversen <martin@graversen.io>
2019-08-18 20:37:15 +0200

    :sparkles: uMod plugin 'TheJudge'

---
+++ src/main/umod/TheJudge.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Oxide.Plugins
+{
+    [Info("The Judge (Ownzone)", "Ownzone", "0.0.1")]
+    [Description("")]
+    class TheJudge : RustPlugin
+    {
+
+    }
+}
