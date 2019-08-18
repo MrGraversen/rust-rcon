4b5c099f49b3a9dabd606e4bb0bbf956c2f15142
Martin Graversen <martin@graversen.io>
2019-08-18 18:19:01 +0200

    Add missing RustEvent descriptor for PlayerSuicideEvent

--- src/main/java/io/graversen/rust/rcon/events/RustEvents.java
+++ src/main/java/io/graversen/rust/rcon/events/RustEvents.java
@@ -5,10 +5,7 @@ import io.graversen.rust.rcon.events.types.BaseRustEvent;
 import io.graversen.rust.rcon.events.types.custom.PlayerDeathEvent;
 import io.graversen.rust.rcon.events.types.game.SaveEvent;
 import io.graversen.rust.rcon.events.types.game.WorldEvent;
-import io.graversen.rust.rcon.events.types.player.ChatMessageEvent;
-import io.graversen.rust.rcon.events.types.player.PlayerConnectedEvent;
-import io.graversen.rust.rcon.events.types.player.PlayerDisconnectedEvent;
-import io.graversen.rust.rcon.events.types.player.PlayerSpawnedEvent;
+import io.graversen.rust.rcon.events.types.player.*;
 
 public enum RustEvents
 {
@@ -17,6 +14,7 @@ public enum RustEvents
     PLAYER_DISCONNECTED_EVENT(PlayerDisconnectedEvent.class, RconMessageTypes.PLAYER_DISCONNECTED),
     PLAYER_DEATH_EVENT(PlayerDeathEvent.class, RconMessageTypes.PLAYER_DEATH),
     PLAYER_SPAWNED_EVENT(PlayerSpawnedEvent.class, RconMessageTypes.PLAYER_SPAWNED),
+    PLAYER_SUICIDE_EVENT(PlayerSuicideEvent.class, RconMessageTypes.SUICIDE_EVENT),
     WORLD_EVENT(WorldEvent.class, RconMessageTypes.WORLD_EVENT),
     SAVE_EVENT(SaveEvent.class, RconMessageTypes.SAVE_EVENT);
 
