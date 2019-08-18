3e8b99eac9899ceffd47d8512259948ce8d5595a
Martin Graversen <martin@graversen.io>
2019-08-18 18:03:47 +0200

    :sparkles: Implement spawn.report parsing and representation

--- src/main/java/io/graversen/rust/rcon/protocol/InfoRcon.java
+++ src/main/java/io/graversen/rust/rcon/protocol/InfoRcon.java
@@ -1,23 +1,23 @@
 package io.graversen.rust.rcon.protocol;
 
 import io.graversen.rust.rcon.objects.RconReceive;
-import io.graversen.rust.rcon.objects.rust.BanInfo;
-import io.graversen.rust.rcon.objects.rust.BuildInfo;
-import io.graversen.rust.rcon.objects.rust.Player;
-import io.graversen.rust.rcon.objects.rust.ServerInfo;
+import io.graversen.rust.rcon.objects.rust.*;
+import io.graversen.rust.rcon.objects.util.Population;
 import io.graversen.rust.rcon.rustclient.IRconClient;
 import io.graversen.trunk.io.serialization.interfaces.ISerializer;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class InfoRcon extends BaseRcon
 {
     private static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(1);
+
+    private static final String SPAWN_REPORT_KEY_SEPARATOR = "\n\t";
+    private static final String SPAWN_REPORT_VALUE_SEPARATOR = "\n\n";
+    private static final int SPAWN_REPORT_POPULATION_READ_AHEAD = "Population: ".length();
+
     private final ISerializer serializer;
 
     InfoRcon(IRconClient rconClient, ISerializer serializer)
@@ -94,5 +94,33 @@ public class InfoRcon extends BaseRcon
         }
 
         return null;
+    }
+
+    public SpawnReport getSpawnReport()
+    {
+        final RconReceive spawnReport = rconClient().sendAsyncBlocking("spawn.report", DEFAULT_TIMEOUT.getSeconds(), TimeUnit.SECONDS);
+        final String message = spawnReport.getMessage();
+
+        final Map<String, Population> populationMap = new HashMap<>();
+
+        int needle = 0;
+        while (needle < message.length() - SPAWN_REPORT_VALUE_SEPARATOR.length())
+        {
+            final int keyNeedle = message.indexOf(SPAWN_REPORT_KEY_SEPARATOR, needle);
+            final int keyReadAhead = needle == 0 ? needle : needle + SPAWN_REPORT_KEY_SEPARATOR.length();
+            final String key = message.substring(keyReadAhead, keyNeedle);
+
+            final int valueNeedle = message.indexOf(SPAWN_REPORT_VALUE_SEPARATOR, keyNeedle);
+            final String value = message.substring(keyNeedle + SPAWN_REPORT_VALUE_SEPARATOR.length(), valueNeedle);
+            final String valueTrimmed = value.substring(SPAWN_REPORT_POPULATION_READ_AHEAD);
+            final String[] populationValues = valueTrimmed.split("/");
+            final var population = new Population(Integer.parseInt(populationValues[0]), Integer.parseInt(populationValues[1]));
+
+            populationMap.put(key, population);
+
+            needle = valueNeedle;
+        }
+
+        return new SpawnReport(populationMap);
     }
 }
