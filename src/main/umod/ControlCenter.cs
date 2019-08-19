7f119f158a7f6029b9dd6d360be947d1a3c8f069
Martin Graversen <martin@graversen.io>
2019-08-19 20:19:07 +0200

    :construction: C# ControlCenter uMod plugin

---
+++ src/main/umod/ControlCenter.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Oxide.Plugins
+{
+    [Info("ControlCenter (Ownzone)", "Ownzone", "0.0.1")]
+    [Description("Exposes controls for various events of the server")]
+    class ControlCenter : RustPlugin
+    {
+        [ConsoleCommand("")]
+        private void x(ConsoleSystem.Arg arg)
+		{
+
+		}
+    }
+}
