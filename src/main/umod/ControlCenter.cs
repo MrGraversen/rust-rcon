37e2104bb5680afa57106d12cb2a8c33843a5d9f
Martin Graversen <martin@graversen.io>
2019-08-19 20:25:25 +0200

    :construction: Basic C# implementation of ControlCenter plugin

--- src/main/umod/ControlCenter.cs
+++ src/main/umod/ControlCenter.cs
@@ -8,10 +8,27 @@ namespace Oxide.Plugins
     [Description("Exposes controls for various events of the server")]
     class ControlCenter : RustPlugin
     {
-        [ConsoleCommand("")]
-        private void x(ConsoleSystem.Arg arg)
+        [ConsoleCommand("cc.chinook")]
+        private void CmdSpawnChinook(ConsoleSystem.Arg arg)
 		{
+			var ch47 = (CH47HelicopterAIController) GameManager.server.CreateEntity("assets/prefabs/npc/ch47/ch47scientists.entity.prefab", new Vector3(0, 200, 0));
+            if (ch47 == null) return;
+            ch47.Spawn();
+		}
 
+		[ConsoleCommand("cc.patrolheli")]
+        private void CmdSpawnPatrolHeli(ConsoleSystem.Arg arg)
+		{
+            var heli = GameManager.server.CreateEntity("assets/prefabs/npc/patrol helicopter/patrolhelicopter.prefab");
+            if (heli == null) return;
+            heli.Spawn();
+		}
+
+		[ConsoleCommand("cc.airdrop")]
+        private void CmdSpawnPatrolHeli(ConsoleSystem.Arg arg)
+		{
+            var entity = GameManager.server.CreateEntity("assets/prefabs/npc/cargo plane/cargo_plane.prefab", new Vector3());
+            entity?.Spawn();
 		}
     }
 }
