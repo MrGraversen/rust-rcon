9e87b4b55ccad8cbc10b755e922efb4dcd339503
Martin Graversen <martin@graversen.io>
2023-09-02 16:41:31 +0200

    :fire: Remove v1 tests

--- src/test/java/io/graversen/v1/rust/rcon/BaseDefaultParserTest.java
+++
@@ -1,8 +0,0 @@
-package io.graversen.v1.rust.rcon;
-
-import io.graversen.v1.rust.rcon.objects.rust.ISteamPlayer;
-
-abstract class BaseDefaultParserTest
-{
-    protected ISteamPlayer defaultPlayer = () -> "1234";
-}
